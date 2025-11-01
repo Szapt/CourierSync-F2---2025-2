4ccef461ced86d12799cee1380ecc66ab1c43f7e
jeonjuani <juan.castro17@udea.edu.co>
2025-11-01 14:18:45 -0500

    Update SecurityConfig.java
    Se modificó la ruta para permitir el acceso a todas las apis de /routes

--- src/main/java/com/couriersync/config/SecurityConfig.java
+++ src/main/java/com/couriersync/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                     "/signup",
                     "/register",
                     "/logout",
-                    "/routes/create-route",
+                    "/routes/**",
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
                     "/swagger-ui.html"
